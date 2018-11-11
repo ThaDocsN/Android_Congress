514a031dc30b76128bbd5d7197394004e7d3321a
ThaDocIzN <rodan72@msn.com>
2018-11-11 13:38:30 -0600

    added a detail activity

---
+++ app/src/main/java/com/lambdaschool/congressdetails/activities/DetailCongressPerson.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.congressdetails.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lambdaschool.congressdetails.R;
+
+public class DetailCongressPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_congress_person);
+    }
+}
--- app/src/main/java/com/lambdaschool/congressdetails/activities/ListActivity.java
+++ app/src/main/java/com/lambdaschool/congressdetails/activities/ListActivity.java
@@ -1,19 +1,63 @@
 package com.lambdaschool.congressdetails.activities;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.lambdaschool.congressdataapiaccess.CongressDao;
+import com.lambdaschool.congressdataapiaccess.CongresspersonOverview;
 import com.lambdaschool.congressdetails.R;
+import com.lambdaschool.congressdetails.viewModel.CongressOverviewViewModel;
+
+import java.util.ArrayList;
 
 
 public class ListActivity extends AppCompatActivity {
+
+    Context context;
+    CongressOverviewViewModel viewModel;
+    LinearLayout parentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        context = this;
+        parentLayout = findViewById(R.id.parentlayout);
+
+        viewModel = ViewModelProviders.of(this).get(CongressOverviewViewModel.class);
+        viewModel.getOverView().observe(this, new Observer<ArrayList<CongresspersonOverview>>() {
+            @Override
+            public void onChanged(@Nullable ArrayList<CongresspersonOverview> congresspersonOverviews) {
+                if (congresspersonOverviews != null){
+                    for (CongresspersonOverview person:congresspersonOverviews){
+                        parentLayout.addView(getDefaultTextView(person));
+
+                    }
+                }
+            }
+        });
+
         CongressDao.getAllMembers();
+    }
+
+    TextView getDefaultTextView(final CongresspersonOverview person){
+        TextView view = new TextView(context);
+        String personName = String.format("%s %s", person.getFirstName(), person.getLastName());
+        String idTag = person.getId();
+        view.setTag(idTag);
+        view.setText(personName);
+        view.setTextSize(28);
+
+        return view;
     }
 }
